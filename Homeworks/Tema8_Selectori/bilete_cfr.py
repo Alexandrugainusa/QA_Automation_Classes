816af4b82d295f6e34a824313f573953180b99ae
alexandrugainusa <alexandruu.gainusa@yahoo.com>
2022-08-02 23:16:13 +0300

    cfr ticket

--- Homeworks/Tema8_Selectori/bilete_cfr.py
+++ Homeworks/Tema8_Selectori/bilete_cfr.py
@@ -17,20 +17,21 @@ driver.find_element(By.ID, 'L2AGLb').click()
 
 # selector by NAME
 search_field = driver.find_element(By.NAME, 'q')
-
+time.sleep(3)
 # cautam cuvantul "harti"
 search_field.send_keys("mersul trenurilor")
 search_field.submit()
+time.sleep(3)
 
 driver.find_element(By.CSS_SELECTOR, ".tF2Cxc > .yuRUbf .LC20lb").click()
-
+time.sleep(3)
 driver.find_element(By.ID, "input-date-departure").click()
 driver.find_element(By.LINK_TEXT, "3").click()
-
+time.sleep(3)
 
 driver.find_element(By.ID, "input-station-departure-name").send_keys("Bucuresti Nord")
-
+time.sleep(3)
 driver.find_element(By.ID, "input-station-arrival-name").send_keys("Busteni")
-
+time.sleep(3)
 driver.find_element(By.XPATH, '//*[@id="form-search"]/button').click()
-
+time.sleep(3)
